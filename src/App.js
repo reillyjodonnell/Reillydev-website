c2fa210cec74f4dc16dfa0c4e94ac9b27e9a5f27
OReally <odonnell.reilly.j@gmail.com>
2021-04-09 23:01:33 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar>
+          <h1>
+            Header here
+          </h1>
+        </Navbar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hey! I'm Reilly.
         </p>
+        <Button>
+          <h2>Click here</h2>
+        </Button>
         <a
           className="App-link"
           href="https://reactjs.org"
