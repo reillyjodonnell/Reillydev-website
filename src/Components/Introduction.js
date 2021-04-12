674c24a9f5d807903a4e481c6b498a3e12b50223
OReally <odonnell.reilly.j@gmail.com>
2021-04-11 22:08:20 -0400

    Created a new component which will hold the intro text

---
+++ src/Components/Introduction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Introduction() {
+    return (
+        <div style={{margin: 'auto'}}>
+            <h2>
+                Hey I'm Reilly
+            </h2>
+        </div>
+    )
+}
--- src/Components/PrimarySection.js
+++ src/Components/PrimarySection.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import '../CSS Components/PrimarySection.css';
-import Navbar from './NavigationBar'
-//This needs to show the first section. Including two main columns 
+import React from "react";
+import "../CSS Components/PrimarySection.css";
+import Navbar from "./NavigationBar";
+import Introduction from './Introduction';
+//This needs to show the first section. Including two main columns
 //The left-most column that shows the main symbols
 //The other column contains the rest of the content
 export default function PrimarySection() {
-    return (
-        <div className='content'>
-            <div className = 'secondary-content'>
-            <h1>
-                This is the secondary content
-            </h1>
-            </div>
-            <div className = 'main-content'>
-                <Navbar/>
-            <h1>
-                This is the main content
-            </h1>
-            </div>
-            
-        </div>
-            
-      
-    )
+  return (
+    <div className="content">
+      <div className="secondary-content">
+        <h1>This is the secondary content</h1>
+      </div>
+      <div className="main-content">
+        <Navbar />
+        <Introduction/>
+      </div>
+    </div>
+  );
 }
