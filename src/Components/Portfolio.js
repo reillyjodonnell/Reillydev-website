ce0d45aed61288552441615782766f0df0269c02
Reilly <reillyodie@gmail.com>
2021-09-06 22:36:32 -0400

    Created a reusable component, project, which expects certain parameters to be passed.

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -2,109 +2,88 @@ import React from "react";
 import Planet from "../Assets/Icons/Planet.svg";
 import Memesfr from "../Assets/Screenshots/Desktop.png";
 import "../CSS Components/Portfolio.css";
+import ReactIcon from "../Assets/Icons/React.svg";
+import JavaScript from "../Assets/Icons/JavaScript.svg";
+import MaterialUI from "../Assets/Icons/MaterialUI.svg";
+import Jest from "../Assets/Icons/Jest.svg";
 import Firebase from "../Assets/Icons/Firebase.svg";
+import Project from "./Project";
 
 export default function Portfolio({ darkMode }) {
   return (
     <>
       <section id="portfolio">
         <div className="portfolio-container">
-          <div className="portfolio-header">
+          <div className="portfolio-header navigation-header">
             <img src={Planet} alt="A picture of a planet" />
+
             <h1>Here's What I've Worked On</h1>
           </div>
-          <div className="project">
-            <div className="project-image">
-              <div className="project-image-container">
-                <img className="project-image-fit" src={Memesfr} />
-              </div>
-            </div>
-            <div className="project-overview">
-              <div className="project-title">
-                <h2>Memesfr</h2>
-              </div>
-              <div className="project-description">
-                <span>
-                  A full stack social media application built with React,
-                  Firebase, and Material-UI.
-                </span>
-              </div>
-              <div className="project-technologies">
-                <span>These are the technologies used</span>
-                <img src={Firebase} />
-              </div>
-              <div className="project-links">
-                <span>These are the links</span>
-              </div>
-            </div>
-          </div>
+          <Project
+            projectImage={Memesfr}
+            projectTitle="Memesfr"
+            projectDescription="A full stack social media application that allows users to
+                  share memes within the community. Users can post, like, and
+                  favorite memes."
+            technologies={[
+              "React",
+              "Firebase",
+              "JavaScript",
+              "Material-UI",
+              "CSS",
+            ]}
+            technologiesIcons={[
+              ReactIcon,
+              Firebase,
+              JavaScript,
+              MaterialUI,
+              Jest,
+            ]}
+          />
+          <Project
+            projectImage={Memesfr}
+            projectTitle="Memesfr"
+            projectDescription="A full stack social media application that allows users to
+                  share memes within the community. Users can post, like, and
+                  favorite memes."
+            technologies={[
+              "React",
+              "Firebase",
+              "JavaScript",
+              "Material-UI",
+              "CSS",
+            ]}
+            technologiesIcons={[
+              ReactIcon,
+              Firebase,
+              JavaScript,
+              MaterialUI,
+              Jest,
+            ]}
+          />
+          <Project
+            projectImage={Memesfr}
+            projectTitle="Memesfr"
+            projectDescription="A full stack social media application that allows users to
+                  share memes within the community. Users can post, like, and
+                  favorite memes."
+            technologies={[
+              "React",
+              "Firebase",
+              "JavaScript",
+              "Material-UI",
+              "CSS",
+            ]}
+            technologiesIcons={[
+              ReactIcon,
+              Firebase,
+              JavaScript,
+              MaterialUI,
+              Jest,
+            ]}
+          />
         </div>
       </section>
-
-      <div
-        className={darkMode ? "about-container-dark" : "about-container-light"}
-      >
-        <div className="about-background">
-          <div className="greeting">
-            <div className="portfolio-header">
-              <div
-                className={
-                  darkMode
-                    ? "greeting-intro-text dark transition"
-                    : "greeting-intro-text transition"
-                }
-              ></div>
-              <div
-                className={
-                  darkMode
-                    ? "about-text dark transition"
-                    : "about-text transition"
-                }
-              >
-                <div className="navigation-header">
-                  <img src={Planet} />
-                  <h1>Here's What I've Built:</h1>
-                </div>
-              </div>
-            </div>
-            <img />
-            <div
-              className={
-                darkMode
-                  ? "greeting-profession-text transition dark "
-                  : "greeting-profession-text transition"
-              }
-            >
-              <span>Project 1</span>
-            </div>
-            <img />
-            <div
-              className={
-                darkMode
-                  ? "greeting-profession-text transition dark "
-                  : "greeting-profession-text transition"
-              }
-            >
-              <span>Project 1</span>
-            </div>
-            <img />
-            <div
-              className={
-                darkMode
-                  ? "greeting-profession-text transition dark "
-                  : "greeting-profession-text transition"
-              }
-            >
-              <span>Project 1</span>
-            </div>
-          </div>
-          <div className="gradient">
-            <div className="about-card-image">
-              <img />
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
---
+++ src/Components/Project.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "../CSS Components/Project.css";
+
+export default function Project(props) {
+  return (
+    <div className="project">
+      <div className="project-image">
+        <div className="project-image-container">
+          <div className="portfolio-image-filter">
+            <img className="project-image-fit" src={props.projectImage} />
+          </div>
+        </div>
+      </div>
+      <div className="project-overview">
+        <div className="project-title">
+          <h2>{props.projectTitle}</h2>
+        </div>
+        <div className="project-description">
+          <span>{props.projectDescription}</span>
+        </div>
+        <div className="project-technologies">
+          <span className="list-of-technology-title">Technologies used:</span>
+          <div className="list-of-technology-icons">
+            {props.technologiesIcons.map((icon, index) => {
+              const text = props.technologies[index];
+              return (
+                <div className="technologies-icon">
+                  <img src={icon} />
+                  <span></span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="project-links">
+          <span>These are the links</span>
+        </div>
+      </div>
+    </div>
+  );
+}
