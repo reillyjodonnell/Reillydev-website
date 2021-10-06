3ab7661bec196e46b33b9f8d11b6bc509b20435f
Reilly <reillyodie@gmail.com>
2021-10-06 18:14:08 -0400

    Added mail icon with an href to my email address.

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,6 +5,7 @@ import {
   faGithub,
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Alien from "../Assets/Icons/Alien.svg";
 import { useTheme } from "../contexts/ThemeContext";
@@ -41,6 +42,13 @@ export default function Skills() {
               href="https://www.linkedin.com/in/reillyjodonnell/"
             >
               <FontAwesomeIcon className="fa-2x" icon={faLinkedin} />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="mailto:odonnell.reilly.j@gmail.com.com?subjectThe%20Force%20is%20Strong%20With%20This%20One"
+            >
+              <FontAwesomeIcon className="fa-2x" icon={faEnvelope} />
             </a>
           </div>
         </div>
